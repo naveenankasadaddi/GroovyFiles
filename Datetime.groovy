cdd70e530f2c2df913540052ebf46d86879da235
nma <naveenank2001@gmail.com>
2023-03-20 11:36:47 +0530

    Save the files

---
+++ Datetime.groovy
@@ -0,0 +1,19 @@
+class Datetime{
+
+
+    static void main(String[] args){
+        Integer i=10
+        Float f=15
+        Float z=f+i
+        println(i)
+        println(f)
+        println(z)
+
+    }
+
+}
+
+
+
+
+
